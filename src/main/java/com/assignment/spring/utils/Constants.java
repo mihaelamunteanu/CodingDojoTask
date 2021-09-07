fa489db206ec0c1ac3625bb0a9e9f622aabd68d7
Mihaela <munteanumihaela@gmail.com>
2021-09-07 19:08:55 +0300

    clean commit

--- src/main/java/com/assignment/spring/utils/Constants.java
+++ src/main/java/com/assignment/spring/utils/Constants.java
@@ -2,7 +2,7 @@ package com.assignment.spring.utils;
 
 public class Constants {
 
-    public static final String APP_ID = "4771ef9d324a76fd9cb8219cb79639b7";
+    public static final String APP_ID = "";
 
     public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={appid}";
     
